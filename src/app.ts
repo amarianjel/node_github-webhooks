5605054fe1b4a4155289dd407cdc200c261ab918
amarianjel <abrm.enginner@gmail.com>
2024-03-07 11:31:28 -0300

    Middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 
 (()=> {
     main();
@@ -11,8 +12,10 @@ function main(){
     const app = express();
     
     app.use( express.json() );
-    
+
     const controller = new GithubController();
+
+    app.use( GithubSha256Middleware.verifySignature );
 
     app.post('/api/github', controller.webhookHandler );
 
